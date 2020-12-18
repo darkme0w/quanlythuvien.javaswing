1bec285f7a2a3678be78ec30616eb635eae4198a
nguyenhaidark <71329524+nguyenhaidark@users.noreply.github.com>
2020-12-18 12:00:29 +0700

    update

--- src/theme/Home.java
+++ src/theme/Home.java
@@ -5,8 +5,6 @@
  */
 package theme;
 
-import java.awt.Component;
-
 /**
  *
  * @author admin
@@ -495,17 +493,12 @@ public class Home extends javax.swing.JFrame {
 
     private void pnlquanlysachMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_pnlquanlysachMouseClicked
         CreateBook createBook = new CreateBook();
-        boolean flag = false;
-        for (Component component : jPanel4.getComponents()) {
-            if (component instanceof CreateBook) {
-                flag = true;
-                break;
-            }
-        }
-        if (!flag) {
+        
+      
             jPanel4.add(createBook);
             createBook.setVisible(true);
-        }
+           
+       
     }//GEN-LAST:event_pnlquanlysachMouseClicked
 
     /**
