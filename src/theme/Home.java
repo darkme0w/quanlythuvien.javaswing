6da1509f00a82fe05933f2b1ff8b0a42cdc08479
nguyenhaidark <71329524+nguyenhaidark@users.noreply.github.com>
2020-12-18 10:35:57 +0700

    book

--- src/theme/Home.java
+++ src/theme/Home.java
@@ -546,7 +546,7 @@ public class Home extends javax.swing.JFrame {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new Home().setVisible(true);
-                
+
             }
         });
     }
