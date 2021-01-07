eb2283d71453daa1855a0458903e308295b2b4ea
nguyenhaidark <71329524+nguyenhaidark@users.noreply.github.com>
2021-01-07 10:33:44 +0700

    penalizelogout

--- src/models/Penalize.java
+++ src/models/Penalize.java
@@ -12,6 +12,39 @@ package models;
 public class Penalize {
     private int readerId;
     private int librarianId;
+    private String readerName;
+    private String librarianName;
+    private String phone;
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getReaderName() {
+        return readerName;
+    }
+
+    public void setReaderName(String readerName) {
+        this.readerName = readerName;
+    }
+
+    public String getLibrarianName() {
+        return librarianName;
+    }
+
+    public Penalize(String readerName, String librarianName, String note) {
+        this.readerName = readerName;
+        this.librarianName = librarianName;
+        this.note = note;
+    }
+
+    public void setLibrarianName(String librarianName) {
+        this.librarianName = librarianName;
+    }
     private String note;
 
     public Penalize() {
